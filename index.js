b351999a120321b002f395a7609b5f7e86d15b9d
Phuong H <phuonghqh@gmail.com>
2016-02-24 13:17:48 +0700

    Support params

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+"use strict";
+
 var semver = require("semver");
 var fs = require("fs");
-
 var args = {};
 try {
   args = require('command-line-args')([
-    {name: 'semver-webpack-plugin-disable', type: Boolean, defaultValue: false}
+    {name: 'semver-webpack-plugin-disable', type: Boolean, defaultValue: false},
+    {name: 'semver-webpack-plugin-inc-args', type: String}
   ]).parse();
 }
 catch (e) {}
@@ -15,15 +17,24 @@ function SemverWebpackPlugin(options) {
   this.options.files = this.options.files || [];
 }
 
+SemverWebpackPlugin.prototype.extractIncArgs = function() {
+  var incArgs = args["semver-webpack-plugin-inc-args"];
+  if (incArgs) {
+    incArgs = incArgs.split(",");
+  }
+  incArgs = incArgs || this.options.incArgs || [];
+  if (incArgs.length > 2) {
+    throw new Error("`incArgs` must have one or two params");
+  }
+  return incArgs;
+};
+
 SemverWebpackPlugin.prototype.apply = function (compiler) {
   if (args["semver-webpack-plugin-disable"]) {
     return;
   }
 
-  var incParams = this.options.incParams || [];
-  if (incParams.length > 2) {
-    throw new Error("`incParams` must have one or two params");
-  }
+  var incArgs = this.extractIncArgs();
 
   var files = this.options.files;
   if (files.length === 0) {
@@ -35,11 +46,11 @@ SemverWebpackPlugin.prototype.apply = function (compiler) {
 
   files.forEach(function (file) {
     var f = require(file);
-    if (incParams.length === 2) {//TODO refactor `inc` params
-      f.version = semver.inc(f.version, incParams[0], incParams[1]);
+    if (incArgs.length === 2) {//TODO refactor `inc` params
+      f.version = semver.inc(f.version, incArgs[0], incArgs[1]);
     }
-    else if (incParams.length === 1) {
-      f.version = semver.inc(f.version, incParams[0]);
+    else if (incArgs.length === 1) {
+      f.version = semver.inc(f.version, incArgs[0]);
     }
     outMap.set(file, f);
     (typeof done === "function") && done(f);
